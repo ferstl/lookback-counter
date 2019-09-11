b0772747a4e28cd290a49c8fd9adf1b9e1c96ed6
Stefan Ferstl <st.ferstl@gmail.com>
2019-09-11 23:04:34 +0200

    Add more tests

--- src/test/java/com/github/ferstl/lookbackcounter/LookBackCounterTest.java
+++ src/test/java/com/github/ferstl/lookbackcounter/LookBackCounterTest.java
@@ -10,13 +10,13 @@ class LookBackCounterTest {
   private static final int LOOK_BACK = 60;
   private static final int INITIAL_VALUE = 45;
 
-  private AtomicLong initialTimestamp;
+  private AtomicLong currentTimestamp;
   private LookBackCounter lookBackCounter;
 
   @BeforeEach
   void beforeEach() {
-    this.initialTimestamp = new AtomicLong(INITIAL_VALUE);
-    this.lookBackCounter = new LookBackCounter(this.initialTimestamp::get, LOOK_BACK);
+    this.currentTimestamp = new AtomicLong(INITIAL_VALUE);
+    this.lookBackCounter = new LookBackCounter(this.currentTimestamp::get, LOOK_BACK);
   }
 
   @Test
@@ -35,10 +35,10 @@ class LookBackCounterTest {
     // act
     this.lookBackCounter.add(1);
 
-    this.initialTimestamp.incrementAndGet();
+    this.currentTimestamp.incrementAndGet();
     this.lookBackCounter.add(1);
 
-    this.initialTimestamp.incrementAndGet();
+    this.currentTimestamp.incrementAndGet();
     this.lookBackCounter.add(1);
 
     // assert
@@ -50,13 +50,13 @@ class LookBackCounterTest {
     // act
     this.lookBackCounter.add(1);
 
-    this.initialTimestamp.addAndGet(LOOK_BACK - INITIAL_VALUE - 1);
+    this.currentTimestamp.addAndGet(LOOK_BACK - INITIAL_VALUE - 1);
     this.lookBackCounter.add(1);
 
-    this.initialTimestamp.incrementAndGet();
+    this.currentTimestamp.incrementAndGet();
     this.lookBackCounter.add(1);
 
-    this.initialTimestamp.incrementAndGet();
+    this.currentTimestamp.incrementAndGet();
     this.lookBackCounter.add(1);
 
     // assert
@@ -64,21 +64,65 @@ class LookBackCounterTest {
   }
 
   @Test
-  void exceedLookBack() {
+  void exceedLookBackWithOverlap() {
     // arrange
     int elapse = LOOK_BACK / 2;
-
-    for (int i = 0; i < LOOK_BACK; i++) {
-      this.lookBackCounter.add(1);
-      this.initialTimestamp.incrementAndGet();
-    }
-
-    this.initialTimestamp.addAndGet(elapse);
+    fillAllBuckets();
 
     // act
+    this.currentTimestamp.addAndGet(elapse);
     this.lookBackCounter.add(1);
 
     // assert
     assertEquals(elapse + 1, this.lookBackCounter.get());
+  }
+
+  @Test
+  void exceedLookBackExactly() {
+    // arrange
+    fillAllBuckets();
+
+    // act
+    this.currentTimestamp.addAndGet(LOOK_BACK);
+    this.lookBackCounter.add(1);
+
+    // assert
+    assertEquals(1, this.lookBackCounter.get());
+  }
+
+  @Test
+  void exceedLookBackMinusOne() {
+    // arrange
+    fillAllBuckets();
+
+    // act
+    this.currentTimestamp.addAndGet(LOOK_BACK - 1);
+    this.lookBackCounter.add(1);
+
+    // assert
+    assertEquals(2, this.lookBackCounter.get());
+  }
+
+  @Test
+  void exceedLookBackPlusOne() {
+    // arrange
+    fillAllBuckets();
+
+    // act
+    this.currentTimestamp.addAndGet(LOOK_BACK + 1);
+
+    // assert
+    assertEquals(0, this.lookBackCounter.get());
+  }
+
+  // Fill all buckets and leave the current timestamp at the last bucket
+  private void fillAllBuckets() {
+    for (int i = 0; i < LOOK_BACK; i++) {
+      this.lookBackCounter.add(1);
+
+      if (i < LOOK_BACK - 1) {
+        this.currentTimestamp.incrementAndGet();
+      }
+    }
   }
 }
